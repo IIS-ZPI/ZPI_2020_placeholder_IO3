a5f942c9f5b47d1bb9491f58116fb03c222c8508
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2020-05-16 18:26:34 +0200

    Categories by id endpoint

---
+++ USASales/USASales/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using USASales.Repositories;
+
+namespace USASales.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoriesRepository _categoriesRepository;
+
+        public CategoriesController(ICategoriesRepository categoriesRepository)
+        {
+            _categoriesRepository = categoriesRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(await _categoriesRepository.GetCategoriesNames());
+        }
+    }
+}
---
+++ USASales/USASales/Repositories/ICategoriesRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using USASales.Models;
+
+namespace USASales.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesNames();
+    }
+
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        private readonly MySqlConnection _connection;
+
+        public CategoriesRepository(MySqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public Task<IEnumerable<Category>> GetCategoriesNames()
+        {
+            return _connection.QueryAsync<Category>("SELECT * FROM Categories");
+        }
+    }
+}
