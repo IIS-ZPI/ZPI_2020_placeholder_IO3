4c6903609c83bd0f01a9abcda9c19c9a7283245d
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2020-05-18 21:33:02 +0200

    Consider threshold when calculating tax

--- USASales/USASales/ProductsService.cs
+++ USASales/USASales/ProductsService.cs
@@ -6,24 +6,28 @@ namespace USASales
     {
         public static DetailedPrice CalculatePrice(Product product, Tax tax)
         {
-            var margin = CalculateMargin(product.WholesalePrice, product.GrossPrice, tax.TaxPercentage);
-            var taxValue = (product.WholesalePrice + margin) * tax.TaxPercentage * 0.01;
+            var taxPercentage = tax.TaxPercentage;
+            var netPrice = product.GrossPrice / (1 + taxPercentage * 0.01);
+
+            if (tax.ThresholdUsd > 0 && netPrice <= tax.ThresholdUsd)
+            {
+                taxPercentage = 0;
+                netPrice = product.GrossPrice;
+            }
+
+            var margin = netPrice - product.WholesalePrice;
+            var taxValue = netPrice * taxPercentage * 0.01;
 
             return new DetailedPrice
             {
                 WholesalePrice = product.WholesalePrice,
                 Margin = margin,
-                TaxPercentage = tax.TaxPercentage,
+                TaxPercentage = taxPercentage,
                 TaxValue = taxValue,
                 GrossPrice = product.GrossPrice,
                 State = tax.State,
                 Threshold = tax.ThresholdUsd
             };
-        }
-
-        private static double CalculateMargin(double basicPrice, double grossPrice, double taxPercentage)
-        {
-            return (grossPrice - basicPrice * (1 + taxPercentage * 0.01)) / (1 + taxPercentage * 0.01);
         }
     }
 }
